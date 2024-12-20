ff124ba26ee18f64256774ebec25b29cf62fbe6d
Random Person <yeyintlwin30@gmail.com>
2024-12-20 17:17:55 -0500

    Commit to the Bit

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
